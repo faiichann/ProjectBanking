f3e829d75de1d2c9617afb381ad02755d8ac10ee
Nichkamon Promputta <fainichkamon@gmail.com>
2020-10-08 21:04:37 +0700

    Initial commit

---
+++ ProjectBanking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectBanking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
