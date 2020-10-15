88a702affb8f4317084158f28bd7fb5898391a92
noojob001 <noojob001@outlook.com>
2020-10-16 00:00:55 +0700

    Add Model Validation

--- ProjectBanking/Models/CustomerModel.cs
+++ ProjectBanking/Models/CustomerModel.cs
@@ -9,13 +9,22 @@ namespace ProjectBanking.Models
 {
     public class Customer
     {
+        [Key]
+        [Required(ErrorMessage = "กรุณากรอกเลขบัตรประชาชน")]
+        [StringLength(13 , ErrorMessage = "กรุณากรอกเลขบัตรประชาชนให้ครบ13หลัก", MinimumLength = 13)]
         public string CustomerID { get; set; }
+        [Required(ErrorMessage = "กรุณากรอกชื่อ - นามสกุล")]
         public string CustomerName { get; set; }
+        [Required(ErrorMessage = "กรุณากรอกที่อยู่ปัจจุบัน")]
         public string CustomerAddress { get; set; }
+        [Phone]
+        [Required(ErrorMessage = "กรุณากรอกหมายเลขโทรศัพท์")]
+        [StringLength(10 , ErrorMessage = "กรุณากรอกเบอร์ติดต่อให้ถูกต้อง" , MinimumLength = 9)]
         public string CustomerPhone { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:MMMM-dd-yy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "กรุณาใส่วันที่")]
         public DateTime CustomerDate { get; set; }
 
         [DataType(DataType.ImageUrl)]
