598cd5f31592180d07b6cbe8f8ecb45adf51266d
ssomoo2 <72564324+ssomoo2@users.noreply.github.com>
2020-10-17 20:32:09 +0700

    ทำการ์ด

--- ProjectBanking/Startup.cs
+++ ProjectBanking/Startup.cs
@@ -50,7 +50,7 @@ namespace ProjectBanking
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Formbank}/{id?}");
+                    pattern: "{controller=Home}/{action=Home1}/{id?}");
             });
         }
     }
--- ProjectBanking/wwwroot/js/site.js
+++ ProjectBanking/wwwroot/js/site.js
@@ -23,7 +23,7 @@ var mybutton = document.getElementById("myBtn");
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
