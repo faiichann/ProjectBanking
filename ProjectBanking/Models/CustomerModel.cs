f2b96c6d893ba08c0d7fd2a796b403dbce7b006a
ssomoo2 <72564324+ssomoo2@users.noreply.github.com>
2020-10-18 00:19:51 +0700

    fixed
    แก้ไขการตกแต่งฟอร์ม

--- ProjectBanking/Models/CustomerModel.cs
+++ ProjectBanking/Models/CustomerModel.cs
@@ -9,17 +9,34 @@ namespace ProjectBanking.Models
 {
     public class Customer
     {
+        [Key]
+        [Required(ErrorMessage = "กรุณากรอกเลขบัตรประชาชน")]
+        [StringLength(13, ErrorMessage = "กรุณากรอกเลขบัตรประชาชนให้ครบ13หลัก", MinimumLength = 13)]
         public string CustomerID { get; set; }
+
+        [Required(ErrorMessage = "กรุณากรอกชื่อบัญชี")]
+        public string CustomerBN { get; set; }
+        [Required(ErrorMessage = "กรุณากรอกชื่อ - นามสกุลผู้ฝาก")]
         public string CustomerName { get; set; }
+        [Required(ErrorMessage = "กรุณากรอกที่อยู่ปัจจุบัน")]
         public string CustomerAddress { get; set; }
+        [Phone]
+        [Required(ErrorMessage = "กรุณากรอกหมายเลขโทรศัพท์")]
+        [StringLength(10, ErrorMessage = "กรุณากรอกเบอร์ติดต่อให้ถูกต้อง", MinimumLength = 9)]
         public string CustomerPhone { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "กรุณาใส่จำนวนเงินฝากเริ่มต้น")]
+        [Range(500.00, double.MaxValue, ErrorMessage = "กรุณาใส่เงินฝากขั้นต่ำ 500 บาท")]
+        public double CustomerMoney { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่วันที่")]
+        [DisplayFormat(DataFormatString = "{0:MMMM-dd-yy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
         public DateTime CustomerDate { get; set; }
 
         [DataType(DataType.ImageUrl)]
         public string CustomerImg { get; set; }
         public string BankSelect { get; set; }
+        /*เพิ่มจำนวนเงินฝากเริ่มต้น / ชื่อบัญชี */
     }
 }
