98b6f303246857c80601c63317e3f3fb186f33a4
noojob001 <noojob001@outlook.com>
2020-10-17 23:25:45 +0700

    Add model complete

--- ProjectBanking/Models/CustomerModel.cs
+++ ProjectBanking/Models/CustomerModel.cs
@@ -13,7 +13,10 @@ namespace ProjectBanking.Models
         [Required(ErrorMessage = "กรุณากรอกเลขบัตรประชาชน")]
         [StringLength(13 , ErrorMessage = "กรุณากรอกเลขบัตรประชาชนให้ครบ13หลัก", MinimumLength = 13)]
         public string CustomerID { get; set; }
-        [Required(ErrorMessage = "กรุณากรอกชื่อ - นามสกุล")]
+
+        [Required(ErrorMessage = "กรุณากรอกชื่อบัญชี")]
+        public string CustomerBN { get; set;}
+        [Required(ErrorMessage = "กรุณากรอกชื่อ - นามสกุลผู้ฝาก")]
         public string CustomerName { get; set; }
         [Required(ErrorMessage = "กรุณากรอกที่อยู่ปัจจุบัน")]
         public string CustomerAddress { get; set; }
@@ -21,6 +24,10 @@ namespace ProjectBanking.Models
         [Required(ErrorMessage = "กรุณากรอกหมายเลขโทรศัพท์")]
         [StringLength(10 , ErrorMessage = "กรุณากรอกเบอร์ติดต่อให้ถูกต้อง" , MinimumLength = 9)]
         public string CustomerPhone { get; set; }
+
+        [Required(ErrorMessage = "กรุณาใส่จำนวนเงินฝากเริ่มต้น")]
+        [Range(500.00, double.MaxValue, ErrorMessage = "กรุณาใส่เงินฝากขั้นต่ำ 500 บาท")]
+        public double CustomerMoney { get; set; }
 
         [Required(ErrorMessage = "กรุณาใส่วันที่")]
         [DisplayFormat(DataFormatString = "{0:MMMM-dd-yy}", ApplyFormatInEditMode = true)]
